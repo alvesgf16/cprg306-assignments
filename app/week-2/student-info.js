4e420b1ead7bf7fa848a0c22d2f57d3e04461745
Gabriel Alves <alvesgabriel1994@gmail.com>
2024-09-09 18:48:25 -0600

    feat: create StudentInfo component

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return(
+    <>
+      <p>Gabriel Alves Ferreira</p>
+      <Link href="https://github.com/alvesgf16">LinkedIn</Link>
+    </>
+  )
+}
